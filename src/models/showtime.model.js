792245351386b531c7acc56ca69cb386fb97c0d0
Phạm Quốc Hưng <96472052+quochung2k3@users.noreply.github.com>
2024-11-18 10:35:36 +0700

    Update

--- src/models/showtime.model.js
+++ src/models/showtime.model.js
@@ -1,40 +1,40 @@
 const mongoose = require("mongoose");
 
 const showtimeSchema = new mongoose.Schema({
-    film_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Film",
-        required: true,
-        trim: true,
-    },
-    branch_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Branch",
-        required: true,
-        trim: true,
-    },
-    screen_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Branch.list_screen",
-        required: true,
-        trim: true,
-    },
-    start_time: {
-        type: Date,
-        required: true,
-    },
-    duration: {
-        type: Number,
-        required: true,
-    },
-    is_active: {
-        type: Boolean,
-        default: true,
-    },
-    created_at: {
-        type: Date,
-        default: Date.now,
-    },
+  film_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Film",
+    required: true,
+    trim: true,
+  },
+  branch_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Branch",
+    required: true,
+    trim: true,
+  },
+  screen_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Branch.list_screen",
+    required: true,
+    trim: true,
+  },
+  start_time: {
+    type: Date,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  is_active: {
+    type: Boolean,
+    default: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Showtime = mongoose.model("Showtime", showtimeSchema);
