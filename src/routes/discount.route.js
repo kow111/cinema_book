71092581ada43039e15685253232ac058e4fbcb1
kow111 <chome710@gmail.com>
2024-11-18 11:03:04 +0700

    apply discount

--- src/routes/discount.route.js
+++ src/routes/discount.route.js
@@ -3,11 +3,13 @@ const {
   getDiscounts,
   postCreateDiscount,
   putUpdateDiscount,
+  postApplyDiscount,
 } = require("../controllers/discount.controller");
 const RouterAPI = express.Router();
 
 RouterAPI.get("/", getDiscounts);
 RouterAPI.post("/", postCreateDiscount);
 RouterAPI.put("/:id", putUpdateDiscount);
+RouterAPI.post("/apply", postApplyDiscount);
 
 module.exports = RouterAPI;
