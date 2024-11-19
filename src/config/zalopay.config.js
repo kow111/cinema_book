3a29e481d636789131e6ccb7f2ffc93aaa76b18a
kow111 <chome710@gmail.com>
2024-11-19 11:04:19 +0700

    zalopay service (Thong)

---
+++ src/config/zalopay.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  app_id: 2553,
+  key1: "PcY4iZIKFCIdgZvA6ueMcMHHUbRLYjPL",
+  key2: "kLtgPl8HHhfvMuDHPwKfgfsY4Ydm9eIz",
+  endpoint: "https://sb-openapi.zalopay.vn/v2/create",
+  callback_url:
+    "https://447d-171-252-189-71.ngrok-free.app/api/v1/zalopay/callback",
+};
